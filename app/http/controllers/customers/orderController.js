5b99afcfed8f3ab2a0aa0ef023c68e9705cb51cb
PramodRai10 <p10pramodr@gmail.com>
2021-07-21 11:08:57 +0530

    Add totalPrice in admin section 6

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -1,29 +1,30 @@
 const Order = require('../../../models/order')
 const moment = require('moment')
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
-function orderController () {
+function orderController() {
     return {
         store(req, res) {
             // Validate request
             const { phone, address, stripeToken, paymentType } = req.body
-            if(!phone || !address) {
-                return res.status(422).json({ message : 'All fields are required' });
+            if (!phone || !address) {
+                return res.status(422).json({ message: 'All fields are required' });
             }
 
             const order = new Order({
                 customerId: req.user._id,
                 items: req.session.cart.items,
                 phone,
-                address
+                address,
+                totalPrice: req.session.cart.totalPrice
             })
             order.save().then(result => {
                 Order.populate(result, { path: 'customerId' }, (err, placedOrder) => {
                     // req.flash('success', 'Order placed successfully')
 
                     // Stripe payment
-                    if(paymentType === 'card') {
+                    if (paymentType === 'card') {
                         stripe.charges.create({
-                            amount: req.session.cart.totalPrice  * 100,
+                            amount: req.session.cart.totalPrice * 100,
                             source: stripeToken,
                             currency: 'inr',
                             description: `Pizza order: ${placedOrder._id}`
@@ -35,38 +36,38 @@ function orderController () {
                                 const eventEmitter = req.app.get('eventEmitter')
                                 eventEmitter.emit('orderPlaced', ord)
                                 delete req.session.cart
-                                return res.json({ message : 'Payment successful, Order placed successfully' });
+                                return res.json({ message: 'Payment successful, Order placed successfully' });
                             }).catch((err) => {
                                 console.log(err)
                             })
 
                         }).catch((err) => {
                             delete req.session.cart
-                            return res.json({ message : 'OrderPlaced but payment failed, You can pay at delivery time' });
+                            return res.json({ message: 'OrderPlaced but payment failed, You can pay at delivery time' });
                         })
                     } else {
                         delete req.session.cart
-                        return res.json({ message : 'Order placed succesfully' });
+                        return res.json({ message: 'Order placed succesfully' });
                     }
                 })
             }).catch(err => {
-                return res.status(500).json({ message : 'Something went wrong' });
+                return res.status(500).json({ message: 'Something went wrong' });
             })
         },
         async index(req, res) {
             const orders = await Order.find({ customerId: req.user._id },
                 null,
-                { sort: { 'createdAt': -1 } } )
+                { sort: { 'createdAt': -1 } })
             res.header('Cache-Control', 'no-store')
             res.render('customers/orders', { orders: orders, moment: moment })
         },
         async show(req, res) {
             const order = await Order.findById(req.params.id)
             // Authorize user
-            if(req.user._id.toString() === order.customerId.toString()) {
+            if (req.user._id.toString() === order.customerId.toString()) {
                 return res.render('customers/singleOrder', { order })
             }
-            return  res.redirect('/')
+            return res.redirect('/')
         }
     }
 }
